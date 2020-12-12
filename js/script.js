40917be7167e259a3e2ca428709e0045064b9dcb
Alka-2022 <70084670+Alka-2022@users.noreply.github.com>
2020-12-12 22:52:42 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+var ml4 = {};
+ml4.opacityIn = [0, 1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 400;
+ml4.durationOut = 300;
+ml4.delay = 500;
+
+var options = {
+    strings: ['Text One', 'Text Two'],
+    typeSpeed: 40,
+    backSpeed: 20,
+    backDelay: 1000,
+    loop: true,
+};
+
+var typed = new Typed('#type', options);
+
+anime.timeline()
+    .add({
+        targets: '.ml4 .letters-1',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-1',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4 .letters-2',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-2',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4 .letters-3',
+        opacity: ml4.opacityIn,
+        scale: ml4.scaleIn,
+        duration: ml4.durationIn
+    }).add({
+        targets: '.ml4 .letters-3',
+        opacity: 0,
+        scale: ml4.scaleOut,
+        duration: ml4.durationOut,
+        easing: "easeInExpo",
+        delay: ml4.delay
+    }).add({
+        targets: '.ml4',
+        opacity: 0,
+        duration: 500,
+        delay: 500
+    });
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $('#main').css('display', 'table');
+        $('.ml4').css('display', 'none');
+    }, 3800);
+});
